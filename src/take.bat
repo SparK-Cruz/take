cc3f58289a80de1b27b5e7ba9281d65eea69a08a
softhardware <39740146+softhardware@users.noreply.github.com>
2018-07-01 20:36:01 -0300

    Add files via upload

---
+++ src/take.bat
@@ -0,0 +1,4 @@
+@echo OFF take
+netsh wlan set hostednetwork mode=allow 
+:: ssid=NomeRede key=Senha
+msgbox"Bem vindo a nossa loja",vbInformation,"Price 12,90"
---
+++ src/take.vbs
@@ -0,0 +1,1 @@
+msgbox"Bem vindo a nossa loja",vbInformation,"Price 12,90"
