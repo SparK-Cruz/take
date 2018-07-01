6f368d2e9efc65d4f16e8c9d4c17df92e921ce38
softhardware <39740146+softhardware@users.noreply.github.com>
2018-07-01 20:49:39 -0300

    Create take.php

---
+++ php/src/take.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+print("R$ 12,90");
+print("Bem vindo a nossa loja"); 
+
+// Comando em javascript que quando conecta no wifi exibe o preço dos produtos
+echo "<script language='take.js'></script>";
+
+?>
