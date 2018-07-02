70a13b0bf3e7f72eb13f0bce7dc60de647fd20f7
softhardware <39740146+softhardware@users.noreply.github.com>
2018-07-01 21:10:58 -0300

    Create take.js

---
+++ nodejs/src/take.js
@@ -0,0 +1,9 @@
+var dialog = require('dialog');
+
+if(true){ wifi  = require('wifi-control'); } 
+
+var here = "Preço 12,90  , Bem vindo a nossa loja";  
+
+var pric = console.log(here); 
+
+dialog.info('Preço 12,90  , Bem vindo a nossa loja');
