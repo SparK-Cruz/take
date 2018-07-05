f84d120c93bc9543612fbca8c19d3a8fe8209ec8
softhardware <39740146+softhardware@users.noreply.github.com>
2018-07-05 17:32:05 -0300

    Create rel2.js

---
+++ rels/rel2.js
@@ -0,0 +1,7 @@
+// Resolução 2 por Guilherme Torquato
+
+try { 
+   navigator.connection.type =='wifi'  || navigator.mozConnection.type =='wifi' || navigator.webkitConnection.type =='wifi' 
+   alert("Preço 12,90 , Bem Vindo a nossa Loja"){
+   catch(e){alert("Por favor conecte -se ao wifi") 
+   }
